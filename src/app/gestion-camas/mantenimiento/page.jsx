e3bda1e16716fcc185353e0eb44ca706693f2743
Kamilo <kamilog17@gmail.com>
2026-08-26 16:24:56 -0500

    feat(gestion-camas): add Mantenimiento route

---
+++ src/app/gestion-camas/mantenimiento/page.jsx
@@ -0,0 +1,5 @@
+import GestionCamasMantenimiento from '@/Components/GestionCamas/GestionCamasMantenimiento/GestionCamasMantenimiento';
+
+export default function GestionCamasMantenimientoPage() {
+  return <GestionCamasMantenimiento />;
+}
